321ee07493178d6c356cc8017c63c95f36a6c6a9
dbozz26 <144434851+dbozz26@users.noreply.github.com>
2023-12-05 21:52:09 +0100

    ex 3 lab2 finished.

--- Homework/Lab/Lab2/dan_ex3.R
+++ Homework/Lab/Lab2/dan_ex3.R
@@ -44,19 +44,18 @@ Proportion of variation explained by the second component:", PC2_var, "\n")
 PCA <- princomp(features)
 
 L <- PCA$loadings
-plot(L[,1], main="Traceplot", xlim=c(0,20))
+plot(L[,1], xlim=c(0,20))
 
 highest5 <- order(L[,1], decreasing=TRUE)[1:5]
 
+# ANSWER:
 # We observe from the plot how the variable "state", the first feature of PC1, 
 # explains most of the data. The other variables carry significantly less 
 # explanation. The other 4 features that contribute the most are features 93 
 # (PctBornSameState), 61 (PctSpeakEnglOnly), 5 (racePctWhite) and 76 
 # (PersPerRentOccHous).
 
-plot(L[,1], L[,2], main="PCA Scores", xlim=c(0,0.005))
-
-# plot to be completed
+plot(L[,1], L[,2], main="PCA Scores")
 
 # 3)
 
@@ -65,7 +64,7 @@ id <- sample(1:n, floor(n*0.5))
 trn <- data[id,]
 tst <- data[-id,]
 
-# scale training and test data
+# scaling
 scaler <- preProcess(trn)
 trainS <- predict(scaler,trn)
 testS <- predict(scaler,tst)
@@ -80,24 +79,50 @@ test_MSE <- mean((testS$ViolentCrimesPerPop - test_pred)^2)
 
 cat("Train mean squared error:", train_MSE, "\nTest mean squared error:", test_MSE)
 
-### answer
+# ANSWER:
+# We observe how the MSE of the test data is significantly bigger than
+# the MSE obtained from train data. This might happen because the model chosen
+# is overfitting our given data.
 
 # 4)
 
+train_new <- as.matrix(trainS[,-101])  # training data - response variable
+train_r <- trainS[,101] # training response variable
 
+test_new <- as.matrix(testS[,-101])  # test data - response variable
+test_r <- trainS[,101] # test response variable
 
+# error vectors for training and test data
+train_e <- c()
+test_e <- c()
 
+costfun <- function(theta_vec){
+  train_cost <- mean(((train_new %*% theta_vec) - train_r)^2)
+  train_e <<- c(train_e, train_cost)
+  test_cost <- mean(((test_new %*% theta_vec) - test_r)^2)
+  test_e <<- c(test_e, test_cost)
+  return(train_cost)
+}
 
+theta0 <- rep(0,100)
+opt <- optim(theta0, method="BFGS", costfun)
 
+opt$val
 
+par(mfrow=c(1,2))
 
+plot(ylab="errors", xlab="iteration",train_e, type = "l", main="Errors")
+lines(test_e, col="red")
+legend("topright", legend = c("Train errors", "Test errors"), col = c("black", "red"), lty = 1, cex = 0.5)
 
+plot(ylab="errors", xlab="iteration",train_e[-c(1:500)][1:2000], type = "l", main="Zoom on errors")
+lines(test_e[-c(1:500)][1:2000], col="red")
+legend("topright", legend = c("Train errors", "Test errors"), col = c("black", "red"), lty = 1, cex = 0.5)
 
-
-
-
-
-
-
+# Looking at the plots we can see how the errors converge to zero
+# after 1700 iterations (approx 1200 in the second graph but considering that we took off
+# the first 500). Hence 1700 is the optimal iteration number to get good results.
+# The following iterations do not significally improve our model
+# and hence can led us to overfitting.
 
 
