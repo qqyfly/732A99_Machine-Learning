8269bd3f195fa7ae1e7a11db908699a2f242f686
Pierre Qi <qiqinyuan@gmail.com>
2023-12-17 21:23:03 +0100

    PDF and Rmd updated ,ALL DONE

--- Homework/Lab/Lab3/Playground1.R
+++ Homework/Lab/Lab3/Playground1.R
@@ -1,56 +1,27 @@
+In this assignment, we will try to predict the temp of Linköping station on date 2000-12-26.
+
+###########################  Init code For Assignment 1 ########################
+rm(list = ls())
+knitr::opts_chunk$set(echo = TRUE)
 library(geosphere)
+set.seed(1234567890)
 
-######################### Kernel Code Area #####################################
-# Gaussian kernel of geo distance
-geo_distance <- function(data, location_interest, h_dist) {
-
-  location <- data.frame(longitude = data$longitude,
-                         latitude  = data$latitude)
-
-  distances <- distHaversine(location_interest,location)
-
-  kernel_result <- exp(-(distances)^2 / (2 * h_dist^2))
-
-  return(c(distances, kernel_result))
-}
-
-# Gaussian kernel of day distance
-day_distance <- function(data, day_interest, h_day) {
-  distances <- as.numeric(difftime(day_interest,data, 
-                          units = "days"))
-
-  kernel_result <- exp(-(distances)^2 / (2 * h_day^2))
-  return(c(distances, kernel_result))
-}
-
-# Gaussian kernel of hour distance
-hour_distance <- function(data, hour_interest, h_hour) {
-  distances <- as.numeric(difftime(strptime(hour_interest, "%H:%M:%S"),
-                              strptime(data$time, "%H:%M:%S"),
-                              units = "hours"))
-  kernel_result <- exp(-(distances)^2 / (2 * h_day^2))
-  return(c(distances, kernel_result))
-}
-
-################################ 1 #################################################
-
-#stations <- read.csv("stations.csv", fileEncoding = "latin1")
-stations <- read.csv("./Homework/Lab/Lab3/stations.csv",
-                    fileEncoding = "latin1")
-
-#temps <- read.csv("temps50k.csv")
+############################ init data ######################################
+stations <- read.csv("./Homework/Lab/Lab3/stations.csv", fileEncoding = "latin1")
 temps <- read.csv("./Homework/Lab/Lab3/temps50k.csv")
-
 st <- merge(stations, temps, by = "station_number")
 
+# define date of interest
+date_interest <- as.Date("2000-12-26")
 
+# filter out data which is later than data of interest 
+st$date <- as.Date(st$date)
+old_sf <- st #for later use
+st <- st[-which(difftime(date_interest, st$date) <= 0),]
 
-date_interest <- as.POSIXlt("2016-12-26")
-
-# Filter out the data before the date of interest
-if (any(as.POSIXlt(st$date) >= date_interest)) { 
-  st <- st[-which(as.POSIXlt(st$date) >= date_interest), ]
-}
+# define station of interest to Linköping
+station_row <- which(stations$station_name == "Linköping") 
+station <- stations$station_name[station_row] 
 
 # split the data to train and test (70/30)
 n <- dim(st)[1]
@@ -58,17 +29,43 @@ id <- sample(1:n, floor(n * 0.7))
 train <- st[id, ]
 test <- st[-id, ]
 
-################################ 1.1 Distance Kernel ###########################
-# Station Name to Linköping 
-#station_row <- which(stations$station_name == "Linköping") 
-station_row <- which(stations$station_name == "Åreskutan Aut") 
+######################### Kernel Code  #####################################
+# Gaussian kernel of geo distance
+geo_distance <- function(data, location_interest, h_dist) {
+  location <- data.frame(longitude = data$longitude,
+                         latitude  = data$latitude)
+  distances <- distHaversine(location_interest,location)
+  kernel_result <- exp(-(distances)^2 / (2 * h_dist^2))
+  return(c(distances, kernel_result))
+}
 
-station <- stations$station_name[station_row] 
+# Gaussian kernel of day distance
+day_distance <- function(data, day_interest, h_day) {
+  distances <- as.numeric(difftime(day_interest,data, 
+                          units = "days"))
+  kernel_result <- exp(-(distances)^2 / (2 * h_day^2))
+  return(c(distances, kernel_result))
+}
+
+# Gaussian kernel of hour distance
+hour_distance <- function(data, hour_interest, h_hour) {
+  
+  diff <- sapply(1:length(hour_interest), function(x){
+              abs(as.numeric(difftime(strptime(hour_interest[x], "%H:%M:%S"), 
+                                      strptime(data$time, "%H:%M:%S")), units="hours"))
+  })
+  
+  distances <- ifelse(diff <= 12, diff, 24-diff)
+  kernel_result <- exp(-(distances)^2 / (2 * h_day^2))
+  return(kernel_result)
+}
+
+################################ 1.1 Distance Kernel ###########################
 
 # These three values are up to the students
-h_distance <- 250000
+h_distance <- 100000
 
-# set coordinates to predict
+# set coordinates to predict(Linköping)
 a <- stations$longitude[station_row]
 b <- stations$latitude[station_row]
 
@@ -78,58 +75,19 @@ k_distance <- sapply(i, function(i){geo_distance(train[i, ], c(a, b), h_distance
 plot(k_distance[1,], k_distance[2,], main = "Distance Gaussian Kernel", xlab = "Distance", ylab = "K")
 
 ################################ 1.2 Day Kernel ###############################
-h_day <- 6000
-k_day <- sapply(i, function(i) day_distance(as.POSIXlt(train$date[i]), date_interest, h_day))
+h_day <- 20
+k_day <- sapply(i, function(i) day_distance(as.Date(train$date[i]), date_interest, h_day))
 plot(k_day[1,], k_day[2,], main = "Day Gaussian Kernel", xlab = "Days", ylab = "K")
-################################ 1.3 Hour Kernel ##############################
-times_interest <- "18:00:00"
 
-# Need to filter here
 h_hour <- 7
+
+# Get the time format we want
+times_interest <- c(paste0("0",seq(4,8,by=2),":00:00"), paste0(seq(10,24,by=2),":00:00"))
+
 k_hour <- sapply(i, function(i) hour_distance(train[i,], times_interest, h_hour))
-plot(k_hour[1,], k_hour[2,], main = "Hour Gaussian Kernel", xlab = "Days", ylab = "K")
-#plot(k_hour[1:11,], k_hour[12:22,], main = "Hour Kernel", xlab = "Diff Hour", ylab = "K")
 
-################################ 1.4 Sum Kernel ##############################
-# TODO: Change the code since it's a copy of the code
+predicted_temp <- sapply(1:length(times_interest), function(x) 
+        sum((k_distance[2,] + k_day[2,] + k_hour[x,]) * train$air_temperature)/ sum(k_distance[2,], k_day[2,], k_hour[x,]))
 
-# setting up the addition kernel which is the *sum* of the 3 kernels distance, day & hour
-k_sum <- matrix(NA, nrow = 11, ncol = N)
-temp_pred_sum <-matrix(NA, nrow = 11, ncol = 1)
-k <- 1
-for (x in 12:22) {
-  # sum the kernels
-  k_sum[k,] <- (k_distance[2,] + k_day[2,] + k_hour[x,])
-  temp_pred_sum[k] <- (k_sum[k,]%*%train$air_temperature)/sum(k_sum[k,])
-  k <- k + 1
-}
-
-N <- nrow(test)
-i <- 1:N
-k_distance <- sapply(i, function(i) k_distance_f(train[i,], a, b, h_dist))
-k_day <- sapply(i, function(i) k_day_f(as.POSIXlt(test$date[i]), date, h_day))
-k_hour <- sapply(i, function(i) k_hour_f(test[i,], times, h_hour))
-
-k_sum <- matrix(NA, nrow = 11, ncol = N)
-temp_pred_sum <-matrix(NA, nrow = 11, ncol = 1)
-k <- 1
-for (x in 12:22) {
-  # sum the kernels
-  k_sum[k,] <- (k_distance[2,] + k_day[2,] + k_hour[x,])
-  temp_pred_sum[k] <- (k_sum[k,]%*%test$air_temperature)/sum(k_sum[k,])
-  k <- k + 1
-}
-k_mult <- matrix(NA, nrow = 11, ncol = N)
-temp_pred_mult <- matrix(NA, nrow = 11, ncol = 1)
-k <- 1
-for (x in 12:22) {
-  k_mult[k,] <- k_distance[2,] * k_day[2,] * k_hour[x,]
-  temp_pred_mult[k] <- (k_mult[k,]%*%test$air_temperature)/sum(k_mult[k,])
-  k <- k + 1
-}
-# combined plot for addition and multiplied kernel
-plot(strptime(times, format = "%H:%M:%S"), temp_pred_mult, ylab = "Predicted Temp", xlab = "Time", 
-     main = paste("Test Data: ", station, date), ylim = c(min(temp_pred_mult, temp_pred_sum), 
-                                                  1+max(temp_pred_mult, temp_pred_sum)), pch = 3)
-points(strptime(times, format = "%H:%M:%S"), temp_pred_sum, col = "red")
-legend(x = "topleft", legend = c("Addition", "Multiplied"), col = c("red", "black"), pch = c(1,3))
+plot(seq(4,24,by=2),predicted_temp, type="l", main = "Temp in Linkoping", xlab = "Time", ylab = "Temp")
+################################ 1.5 Multi Kernel ###############################
--- Homework/Lab/Lab3/Playground3.R
+++ Homework/Lab/Lab3/Playground3.R
@@ -1,86 +1,122 @@
-### EX 3
 
-## 3.1
-
-library(neuralnet)
-
-set.seed(1234567890)
-
-Var <- runif(500, 0, 10)
-mydata <- data.frame(Var, Sin=sin(Var))
-tr <- mydata[1:25,] # Training
-te <- mydata[26:500,] # Test
-
-
-# Random initialization of the weights in the interval [-1, 1]
-#winit <- # Your code here
-nn <- neuralnet(data = tr, formula = Sin ~ Var, hidden = c(10))
-
-# Plot of the training data (black), test data (blue), and predictions (red)
-plot(tr, cex=1)
-points(te, col = "blue", cex=1)
-points(te[,1], predict(nn,te), col="red", cex=1)
-
-# ANSWER: We observe in the plot that the predictions from the neural net (red 
-# dots ) seem to be generally good predictions for the sine function.
-
-## 3.2
-
-# linear activation function
-
-lin_nn <- neuralnet(data=tr, formula = Sin ~ Var, hidden = c(10), 
-                    act.fct = function(x) {x})
-# ReLU activation function ## NOT WORKING
-
-ReLU_nn <- neuralnet(data = tr, formula = Sin ~ Var, hidden = c(10),
-                     act.fct = function(x) {max(0,x)})
-# softplus activation function
-
-softplus_nn <- neuralnet(data = tr, formula = Sin ~ Var, hidden = c(10),
-                         act.fct = function(x) {log(1 + exp(x))})
-
-plot(tr, cex=1)
-points(te, col = "blue", cex=1)
-points(te[,1], predict(lin_nn,te), col="red", cex=1)
-points(te[,1], predict(softplus_nn,te), col="yellow", cex=1)
-
-# ANSWER: For some reasons the ReLU activation function does not seem to work (
-# will wait for feedback), while the other two functions give different results.
-# The linear activation function produces poor predictions, while the softplus
-# produces optimal predicted values.
-
-## 3.3
-
-
-
+###########################  Init code For Assignment 3 ########################
 rm(list = ls())
 knitr::opts_chunk$set(echo = TRUE)
 library(neuralnet)
 set.seed(1234567890)
-```
 
 ### 3.1 
-We observe in the plot that the predictions from the neural net seem to be generally good predictions for the sine function.
 
-```{r 3.1,echo=FALSE}
-Var <- runif(500, 0, 10)
-mydata <- data.frame(Var, Sin=sin(Var))
-tr <- mydata[1:25,] # Training
-te <- mydata[26:500,] # Test
+###########################  3.1 ################################################
+rand_var <- runif(500, 0, 10)
+df <- data.frame(rand_var, Sin=sin(rand_var))
+train <- df[1:25,] # Training
+test <- df[26:500,] # Test
 
 # Random initialization of the weights in the interval [-1, 1]
 winit <- runif(31, -1, 1)
-nn <- neuralnet(data = tr, formula = Sin ~ Var, hidden = c(10))
+nn <- neuralnet(data = train, formula = Sin ~ rand_var, hidden = c(10))
 
 # Plot of the training data (black), test data (blue), and predictions (red)
-plot(tr,  col = "black", cex=1)
-points(te, col = "blue", cex=1)
-points(te[,1], predict(nn,te), col="red", cex=1)
+plot(train,  col = "black", cex=1, main="Default")
+points(test, col = "blue", cex=1)
+points(test[,1], predict(nn,test), col="red", cex=1)
+legend(x = 6.5, y = -0.6, legend = c("Training data", "Test data",
+"Pred"), col = c("black", "blue", "red"), cex = 0.7,pch = 1)
+
+### 3.2 
+
+
+###########################  3.2.1 Linear #######################################
+
+#Linear
+h1 <- function(x) {
+  x
+} 
+
+nn1 <- neuralnet(data = train, formula = Sin ~ rand_var, hidden = c(10), act.fct = h1)
+
+# Plot of the training data (black), test data (blue), and predictions (red)
+plot(train,  col = "black", cex=1, main="Linear")
+points(test, col = "blue", cex=1)
+points(test[,1], predict(nn1,test), col="red", cex=1)
+legend(x = 6.5, y = -0.6, legend = c("Training data", "Test data",
+"Pred"), col = c("black", "blue", "red"), cex = 0.7,pch = 1)
+
+
+###########################  3.2.2 Relu #######################################
+
+#ReLU
+h2 <- function(x) {
+  # max(0,x) not working
+  ifelse(x>=0,x,0)
+}
+
+
+nn2 <- neuralnet(formula = Sin ~ rand_var, data = train, hidden = 10,
+startweights = winit, act.fct = h2)
+
+# Plot of the training data (black), test data (blue), and predictions (red)
+plot(train,  col = "black", cex=1,main="Relu")
+points(test, col = "blue", cex=1)
+points(test[,1], predict(nn2,test), col="red", cex=1)
 legend(x = 6.5, y = -0.6, legend = c("Training data", "Test data",
 "Pred"), col = c("black", "blue", "red"), cex = 0.7,pch = 1)
 
 
 
+###########################  3.2.3 Softplus #######################################
+
+#Softplus
+h3 <- function(x) {
+  log(1+exp(x))
+}
+
+nn3 <- neuralnet(formula = Sin ~ rand_var, data = train, hidden = 10,
+startweights = winit, act.fct = h3)
+
+# Plot of the training data (black), test data (blue), and predictions (red)
+plot(train,  col = "black", cex=1,main="Softplus")
+points(test, col = "blue", cex=1)
+points(test[,1], predict(nn3,test), col="red", cex=1)
+legend(x = 6.5, y = -0.6, legend = c("Training data", "Test data",
+"Pred"), col = c("black", "blue", "red"), cex = 0.7,pch = 1)
 
 
+
+### 3.3
+
+
+###########################  3.3  #######################################
+# Sample 500 points
+set.seed(1234567890)
+rand_var <- runif(500, min = 0, max = 50) 
+df <- data.frame(rand_var, Sin = sin(rand_var))
+plot(train, cex = 2, xlim = c(0, 50), ylim = c(-6, 2))
+points(df, col = "blue", cex = 1)
+points(df[, 1], predict(nn, df), col = "red", cex = 1)
+plot(nn)
+
+
+###########################  3.4  #######################################
+weight_1 <- nn$weights[[1]][[1]][2,]
+bias_1 <- nn$weights[[1]][[1]][1,]
+sigmoid_val <- sigmoid(weight_1 * 100 + bias_1)
+
+bias_2 <- nn$weights[[1]][[2]][1,]
+weight_2 <- nn$weights[[1]][[2]][2:11,]
+cat("The value will converge to:", weight_2 %*% sigmoid_val + bias_2, "\n")
+
+###########################  3.5  #######################################
+
+# Sample 500 points
+Var <- runif(500, min = 0, max = 10) 
+df <- data.frame(Sin = sin(Var), Var)
+nn4 <- neuralnet(formula = Var ~ Sin, data = df, hidden = 10, startweights = winit, threshold = 0.1)
+pred_4 <- predict(nn4, df)
+
+plot(df, cex = 2, col = "blue")
+points(df[, 2], pred_4, col = "red", cex = 1)
+legend(x = -1, y = 8.5, legend = c("Training data", "NN Pred"),
+col = c("blue", "red"), cex = 1, pch = 1)
 
